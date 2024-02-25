a7af347ce28ba8be8ef5f9cbba5b54b1c432939c
timf34 <farrelti@tcd.ie>
2024-02-25 02:36:56 +0000

    cleanup

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,10 +1,5 @@
 console.log("Service worker loaded");
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log('Tab update detected', tabId, changeInfo);
-    console.log(tab.url);
-    if (tab.url && tab.url.includes('chat.openai.com')) {
-        console.log('ChatGPT URL detected:', tab.url);
-    }
     if (changeInfo.url && changeInfo.url.includes('chat.openai.com/c/')) {
         console.log('New ChatGPT conversation detected:', changeInfo.url);
     }
